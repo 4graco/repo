ce89844f381d70d8eaa61a574e25af3e06566180
4graco <gayk.gevorkyan@gmail.com>
2013-12-31 21:34:02 -0800

    WinterAndCandies

---
+++ tc/src/main/java/com/topcoder/srm601/WinterAndCandies.java
@@ -0,0 +1,17 @@
+package com.topcoder.srm601;
+
+public class WinterAndCandies {
+	public int getNumber(int[] types) {
+		int[] countArr = new int[50];
+		for (int i = 0; i < types.length; i++) {
+			countArr[types[i] - 1]++;
+		}
+		int sum = countArr[0];
+		int mult = countArr[0];
+		for (int i = 1; sum != 0 && countArr[i] != 0 && i < countArr.length; i++) {
+			mult *= countArr[i];
+			sum += mult;
+		}
+		return sum;
+	}
+}
---
+++ tc/src/test/java/com/topcoder/srm601/WinterAndCandiesTest.java
@@ -0,0 +1,40 @@
+package com.topcoder.srm601;
+
+import static junit.framework.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class WinterAndCandiesTest {
+
+	@Parameters
+	public static Collection<int[][]> data() {
+		return Arrays
+				.asList(new int[][][] { { { 3 }, { 1, 3, 2 } },
+						{ { 4 }, { 1, 1, 2 } },
+						{ { 9 }, { 1, 3, 2, 5, 7, 4, 5, 4 } },
+						{ { 62 }, { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 } },
+						{ { 0 }, { 2 } } });
+	}
+
+	int[] data;
+	int expectedResult;
+
+	public WinterAndCandiesTest(int expectedResult[], int[] data) {
+		this.expectedResult = expectedResult[0];
+		this.data = data;
+	}
+
+	@Test
+	public void testGetNumber() {
+		WinterAndCandies winterAndCandies = new WinterAndCandies();
+		assertEquals(expectedResult, winterAndCandies.getNumber(data));
+	}
+
+}
