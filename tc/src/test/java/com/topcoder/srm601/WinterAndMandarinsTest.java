b6ca517b903c18643811d9605e50e2535d59085a
4graco <gayk.gevorkyan@gmail.com>
2014-01-03 23:16:19 -0800

    commit

--- tc/src/test/java/com/topcoder/srm601/WinterAndMandarinsTest.java
+++ tc/src/test/java/com/topcoder/srm601/WinterAndMandarinsTest.java
@@ -2,19 +2,40 @@ package com.topcoder.srm601;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class WinterAndMandarinsTest {
+
+	@Parameters
+	public static Collection<int[][]> data() {
+		return Arrays.asList(new int[][][] { { { 10 }, { 10, 20, 30 }, { 2 } },
+				{ { 3 }, { 4, 7, 4 }, { 3 } },
+				{ { 2 }, { 4, 1, 2, 3 }, { 3 } },
+				{ { 1 }, { 5, 4, 6, 1, 9, 4, 2, 7, 5, 6 }, { 4 } },
+				{ { 27 }, { 47, 1000000000, 1, 74 }, { 2 } } });
+	}
+
+	int expectedResult;
+	int[] bags;
+	int K;
+
+	public WinterAndMandarinsTest(int[] expectedResult, int[] bags, int[] K) {
+		this.expectedResult = expectedResult[0];
+		this.bags = bags;
+		this.K = K[0];
+	}
 
 	@Test
 	public void testGetNumber() {
 		WinterAndMandarins wam = new WinterAndMandarins();
-		assertEquals(10, wam.getNumber(new int[] { 10, 20, 30 }, 2));
-		assertEquals(3, wam.getNumber(new int[] { 4, 7, 4 }, 3));
-		assertEquals(2, wam.getNumber(new int[] { 4, 1, 2, 3 }, 3));
-		assertEquals(1,
-				wam.getNumber(new int[] { 5, 4, 6, 1, 9, 4, 2, 7, 5, 6 }, 4));
-		assertEquals(27, wam.getNumber(new int[] { 47, 1000000000, 1, 74 }, 2));
+		assertEquals(expectedResult, wam.getNumber(bags, K));
 	}
 
 }
